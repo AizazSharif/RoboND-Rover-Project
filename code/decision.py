fix: Change direction of rotation in random mode so it goes same way as in stop mode, else they work to cancel each other out

code/decision.py
@@ -90,12 +90,11 @@ def decision_step(Rover):
                     Rover.throttle = 0
                 Rover.brake = 0
                 # Set steering to average angle clipped to the range +/- 15
-                if random.random() > 0.02:
+                if random.random() > 0.01:
                     Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
                 else:
                     Rover.mode = 'random'
             # If there's a lack of navigable terrain pixels then go to 'stop' mode
-            # Also if the rover has stopped moving, i.e. because stuck
             elif len(Rover.nav_angles) < Rover.stop_forward:
                     # Set mode to "stop" and hit the brakes!
                     Rover.throttle = 0
@@ -127,17 +126,16 @@ def decision_step(Rover):
                     # Release the brake
                     Rover.brake = 0
                     # Set steer to mean angle
-                    if random.random() > 0.1:
-                        Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
-                    else:
-                        Rover.steer = random.randint(-30,30)
+                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
                     Rover.mode = 'forward'
 
         elif Rover.mode == 'random':
-            if random.random() > 0.9:
+            if random.random() > 0.95:
                 Rover.mode = 'forward'
             else:
-                Rover.steer = 15
+                Rover.throttle = 0
+                Rover.brake = 0
+                Rover.steer = -15
 
     # Just to make the rover do something 
     # even if no modifications have been made to the code