Fix major bug in coordinate rotation function

code/decision.py
@@ -70,7 +70,7 @@ def decision_step(Rover):
         #print(Rover.rocks_dists)
         
         #if there is a rock, move *slowly* directly towards it
-        Rover.throttle = 0.05
+        Rover.throttle = 0.2
         Rover.steer = np.clip(np.mean(Rover.rocks_angles * 180/np.pi), -15, 15)
 
 
@@ -91,7 +91,7 @@ def decision_step(Rover):
                 Rover.brake = 0
                 # Set steering to average angle clipped to the range +/- 15
                 if random.random() > 0.01:
-                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi) +13, -15, 15)
+                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi) +15, -15, 15)
                     #Rover.steer = np.clip(np.percentile(Rover.nav_angles * 180/np.pi, 75), -15, 15) #aim to make wall-follower
                 else:
                     Rover.mode = 'random'
@@ -119,7 +119,7 @@ def decision_step(Rover):
                     # Release the brake to allow turning
                     Rover.brake = 0
                     # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
-                    Rover.steer = -15 # Could be more clever here about which way to turn
+                    Rover.steer = -10 # Could be more clever here about which way to turn
                 # If we're stopped but see sufficient navigable terrain in front then go!
                 if len(Rover.nav_angles) >= Rover.go_forward:
                     # Set throttle back to stored value
@@ -127,7 +127,7 @@ def decision_step(Rover):
                     # Release the brake
                     Rover.brake = 0
                     # Set steer to mean angle
-                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi) + 13, -15, 15)
+                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi) + 15, -15, 15)
                     Rover.mode = 'forward'
 
         elif Rover.mode == 'random':
@@ -136,7 +136,7 @@ def decision_step(Rover):
             else:
                 Rover.throttle = 0
                 Rover.brake = 0
-                Rover.steer = -15
+                Rover.steer = -10
 
     # Just to make the rover do something 
     # even if no modifications have been made to the code

code/drive_rover.py
@@ -58,15 +58,15 @@ class RoverState():
         self.rocks_dists = None # Distances of rocks pixels
         self.ground_truth = ground_truth_3d # Ground truth worldmap
         self.mode = 'forward' # Current mode (can be forward or stop)
-        self.throttle_set = 0.15 # Throttle setting when accelerating
-        self.brake_set = 7 # Brake setting when braking
+        self.throttle_set = 0.35 # Throttle setting when accelerating
+        self.brake_set = 10 # Brake setting when braking
         # The stop_forward and go_forward fields below represent total count
         # of navigable terrain pixels.  This is a very crude form of knowing
         # when you can keep going and when you should stop.  Feel free to
         # get creative in adding new fields or modifying these!
         self.stop_forward = 50 # Threshold to initiate stopping
         self.go_forward = 500 # Threshold to go forward again
-        self.max_vel = 2 # Maximum velocity (meters/second)
+        self.max_vel = 3 # Maximum velocity (meters/second)
         # Image output from perception step
         # Update this image to display your intermediate analysis steps
         # on screen in autonomous mode
@@ -81,8 +81,8 @@ class RoverState():
         self.near_sample_count = 0
         self.pick_up = False # Set to True to trigger rock pickup
         self.count = 0
-        self.timeout_after_pickup = 200
-        self.close_to_goal_threshold = 5
+        self.timeout_after_pickup = 400
+        self.close_to_goal_threshold = 10
         self.visited = np.ones((200, 200, 1), dtype=np.float)
 # Initialize our rover 
 Rover = RoverState()
@@ -167,7 +167,7 @@ def send_control(commands, image_string1, image_string2):
 # Define a function to send the "pickup" command 
 def send_pickup():
     print("Picking up")
-    Rover.number_samples_collected += 1
+    #Rover.number_samples_collected += 1
     pickup = {}
     sio.emit(
         "pickup",

code/perception.py
@@ -73,8 +73,8 @@ def rotate_pix(xpix, ypix, yaw):
     # Convert yaw to radians
     yaw_rad = yaw * np.pi / 180
     # Apply a rotation
-    xpix_rotated = xpix * np.cos(yaw_rad) - np.sin(yaw_rad)
-    ypix_rotated = xpix * np.sin(yaw_rad) + np.cos(yaw_rad)
+    xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
+    ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
     # Return the result  
     return xpix_rotated, ypix_rotated
 
@@ -152,7 +152,7 @@ def perception_step(Rover):
 
     obstacles_x_world, obstacles_y_world = pix_to_world(obstacles_x, obstacles_y, Rover.pos[0], Rover.pos[1], Rover.yaw, world_size=200, scale=10)
     rocks_x_world, rocks_y_world = pix_to_world(rocks_x, rocks_y, Rover.pos[0], Rover.pos[1], Rover.yaw, world_size=200, scale=10)
-    navigable_x_world, navigable_y_world = pix_to_world(navigable_x, obstacles_y, Rover.pos[0], Rover.pos[1], Rover.yaw, world_size=200, scale=10)
+    navigable_x_world, navigable_y_world = pix_to_world(navigable_x, navigable_y, Rover.pos[0], Rover.pos[1], Rover.yaw, world_size=200, scale=10)
 
     #new_navigable_x, new_navigable_y = [p for p in (navigable_x_world, navigable_y_world) if p not in Rover.visited]
 
@@ -161,14 +161,14 @@ def perception_step(Rover):
         #          Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
         #          Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
 
-    roll_limit = 0.2
-    pitch_limit = 0.2
+    roll_limit = 0.4
+    pitch_limit = 0.4
     # Don`t map if roll or pitch are too large (to increase fidelity)
     if Rover.roll < roll_limit or Rover.roll > 360 - roll_limit:
         if Rover.pitch < pitch_limit or Rover.pitch > 360 - pitch_limit:
-            Rover.worldmap[obstacles_y_world, obstacles_x_world, 0] += 1
-            Rover.worldmap[rocks_y_world, rocks_x_world, 1] += 1
-            Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
+            Rover.worldmap[obstacles_y_world, obstacles_x_world, 0] = 255
+            Rover.worldmap[rocks_y_world, rocks_x_world, 1] = 255
+            Rover.worldmap[navigable_y_world, navigable_x_world, 2] = 255
 
     # 8) Convert rover-centric pixel positions to polar coordinates
     # Update Rover pixel distances and angles